6993c4f3f8088d90036cf4a4de6e6fc2ab773634
César García Cabeza <cesareogarcia@hotmail.es>
2020-03-11 11:56:32 +0100

    Hot fix

--- Lab3/src/ast/types/RecordType.java
+++ Lab3/src/ast/types/RecordType.java
@@ -21,17 +21,15 @@ public class RecordType extends ASTAbstractNode implements Type {
     private List<RecordField> checkFields(List<RecordField> fields)
     {
         List<String> uniquesNames = new ArrayList<String>();
-        List<RecordField> returned = new ArrayList<RecordField>();
         for (RecordField rf : fields) {
             if(uniquesNames.contains(rf.getName()))
                 new ErrorType(rf.getLine(),rf.getColumn(),"Duplicate RecordField name "+rf.getName());
             else
             {
-                returned.add(rf);
                 uniquesNames.add(rf.getName());
             }
         }
-        return returned;
+        return fields;
     }
 
     @Override
